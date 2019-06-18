5c5fb2113bbdf51ae462d4ac1586a318a9e7d56b
Roberto Cazzaniga <rcazzaniga@urmetcns.it>
2019-06-18 15:03:32 +0200

    Platform - singleScreen - creazione personaggio - canvasloop

--- src/app/components/blog/platform/single-screen/creazione-personaggio/creazione-personaggio.component.ts
+++ src/app/components/blog/platform/single-screen/creazione-personaggio/creazione-personaggio.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-creazione-personaggio',
   templateUrl: './creazione-personaggio.component.html',
   styleUrls: ['./creazione-personaggio.component.scss']
 })
-export class CreazionePersonaggioComponent implements OnInit {
+export class CreazionePersonaggioComponent implements AfterViewInit {
 
   constructor() { }
+  @ViewChild('canvasLoop') canvasLoop: ElementRef;
+  // @ViewChild('canvasIntro') canvasIntro: ElementRef;
+  // @ViewChild('canvasIntroFinal') canvasIntroFinal: ElementRef;
+  playerWidth: number;
+  playerHeight: number;
+  playerX: number;
+  playerY: number;
 
+  contextLoop: CanvasRenderingContext2D;
+  // private contextIntro: CanvasRenderingContext2D;
+  // private contextIntroFinal: CanvasRenderingContext2D;
+
+  ngAfterViewInit() {
+    this.contextLoop = (this.canvasLoop.nativeElement as HTMLCanvasElement).getContext('2d');
+    // this.contextIntro = (this.canvasIntro.nativeElement as HTMLCanvasElement).getContext('2d');
+    // this.contextIntroFinal = (this.canvasIntroFinal.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.canvasSize(this.canvasLoop);
+    // this.canvasSize(this.canvasIntro);
+    // this.canvasSize(this.canvasIntroFinal);
+    this.drawLoop();
+    // this.drawIntro();
+    // this.drawIntroFinal();
+  }
+
+  canvasSize(canvas: ElementRef) {
+    canvas.nativeElement.width = 600;
+    canvas.nativeElement.height = 400;
+  }
   ngOnInit() {
   }
 
@@ -49,5 +76,17 @@ export class CreazionePersonaggioComponent implements OnInit {
     }
     `;
 
+  drawLoop() {
+    this.playerWidth = 50;
+    this.playerHeight = 100;
+    this.playerX = 100;
+    this.playerY = this.canvasLoop.nativeElement.height - this.playerHeight;
+    this.loop();
+  }
+
+  loop() {
+    this.contextLoop.fillRect(this.playerWidth, this.playerHeight, this.playerX, this.playerY);
+    requestAnimationFrame(this.loop);
+  };
 
 }
