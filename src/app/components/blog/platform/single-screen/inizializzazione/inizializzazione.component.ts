8a488c78d562cd3d3159fb6063ae26c693c6250f
Roberto Cazzaniga <rcazzaniga@urmetcns.it>
2019-06-18 12:06:20 +0200

    Platform - singleScreen - init - canvasIntroFinal

--- src/app/components/blog/platform/single-screen/inizializzazione/inizializzazione.component.ts
+++ src/app/components/blog/platform/single-screen/inizializzazione/inizializzazione.component.ts
@@ -11,16 +11,22 @@ export class InizializzazioneComponent implements AfterViewInit {
 
   @ViewChild('canvasFillrect') canvasFillrect: ElementRef;
   @ViewChild('canvasIntro') canvasIntro: ElementRef;
+  @ViewChild('canvasIntroFinal') canvasIntroFinal: ElementRef;
 
   private contextFillrect: CanvasRenderingContext2D;
   private contextIntro: CanvasRenderingContext2D;
+  private contextIntroFinal: CanvasRenderingContext2D;
 
   ngAfterViewInit() {
-    this.contextFillrect = (this.canvasIntro.nativeElement as HTMLCanvasElement).getContext('2d');
-    this.contextIntro = (this.canvasFillrect.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.contextFillrect = (this.canvasFillrect.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.contextIntro = (this.canvasIntro.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.contextIntroFinal = (this.canvasIntroFinal.nativeElement as HTMLCanvasElement).getContext('2d');
     this.canvasSize(this.canvasFillrect);
     this.canvasSize(this.canvasIntro);
-    this.draw();
+    this.canvasSize(this.canvasIntroFinal);
+    this.drawFillrect();
+    this.drawIntro();
+    this.drawIntroFinal();
   }
 
   canvasSize(canvas: ElementRef) {
@@ -45,9 +51,51 @@ export class InizializzazioneComponent implements AfterViewInit {
     context.textAlign = "center";
     context.fillText("Platform Single Screen", canvas.width / 2, canvas.height / 2);`;
 
-  private draw() {
+  private drawFillrect() {
     this.contextFillrect.fillStyle = "#ffad6a";
     this.contextFillrect.fillRect(10, 10, 580, 380);
   };
+  private drawIntro() {
+    this.contextIntro.fillStyle = "#ffad6a";
+    this.contextIntro.fillRect(10, 10, 580, 380);
+
+    this.contextIntro.font = "50px Impact";
+    this.contextIntro.fillStyle = "#6a8aff";
+    this.contextIntro.textAlign = "center";
+    this.contextIntro.fillText("Platform Single Screen", this.canvasIntro.nativeElement.width / 2, this.canvasIntro.nativeElement.height / 2);
+  }
+
+  private drawIntroFinal() {
+    this.contextIntroFinal.fillStyle = "#ffad6a";
+    this.contextIntroFinal.fillRect(10, 10, 580, 380);
+
+    this.contextIntroFinal.font = "50px Impact";
+    this.contextIntroFinal.fillStyle = "#5E41CC";
+    this.contextIntroFinal.textAlign = "center";
+    this.contextIntroFinal.fillText("Platform Single Screen", this.canvasIntroFinal.nativeElement.width / 2, this.canvasIntroFinal.nativeElement.height / 2);
+
+    this.contextIntroFinal.fillStyle = "#000";
+    this.contextIntroFinal.fillRect(
+      this.canvasIntroFinal.nativeElement.width / 2 - 95,
+      this.canvasIntroFinal.nativeElement.height / 2 + 30,
+      200,
+      80
+    );
+
+    this.contextIntroFinal.fillStyle = "#85FF88";
+    this.contextIntroFinal.fillRect(
+      this.canvasIntroFinal.nativeElement.width / 2 - 100,
+      this.canvasIntroFinal.nativeElement.height / 2 + 35,
+      200,
+      80
+    );
+
+
+    this.contextIntroFinal.font = "50px Impact";
+    this.contextIntroFinal.fillStyle = "#B37039";
+    this.contextIntroFinal.textAlign = "center";
+    this.contextIntroFinal.fillText("START", this.canvasIntroFinal.nativeElement.width / 2, this.canvasIntroFinal.nativeElement.height / 2 + 95);
+
+  }
 
 }
